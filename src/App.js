5098539dcfc423e7842e3c1c6ffdf503e533f813
Yaser <yaser.ali32@hotmail.co.uk>
2021-08-14 16:11:27 +0100

    another commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import './App.css';
 import axios from 'axios';
 import React from 'react';
-import Select from 'react-select';
+// import Select from 'react-select';
 
 import ReactDom from 'react-dom';
 
+import './App.css';
 
 import Breedlist from './component/BreedList';
 
-const options = [
-    { value: '1', label: '1' },
-    { value: '2', label: '2' },
-    { value: '3', label: '3' },
-];
-
-const options2 = [
-    { value: '1', label: '1' },
-    { value: '2', label: '2' },
-    { value: '3', label: '3' },
-];
 
 
 class App extends React.Component {
@@ -29,19 +18,10 @@ class App extends React.Component {
             breed: [""],
             select: "",
             dogs: [],
-            selectNum: []
-        }
+            selectVal: []
+        };
     }
 
-
-    state = {
-        selectedOption: null,
-      };
-      handleChange = (selectedOption) => {
-        this.setState({ selectedOption }, () =>
-          console.log(`Option selected:`, this.state.selectedOption)
-        );
-      };
 
     getDogImage = () => {
         const { select } = this.state;
@@ -72,24 +52,38 @@ class App extends React.Component {
                 console.log("error fetching list");
             });
     }
+    getSubBreed = () => {
+        const { Sub } = this.state;
+        axios
+            .get("https://dog.ceo/api/breeds/hound/list")
+            .then(response => {
+                this.setState({
+                    Sub: Sub.concat(response.data.message)
+                })
+            })
+            .catch(err => {
+                console.log("error fetching list");
+            });
+    }
 
-    //   getThreeBreedImg = () => {
-    //       const {select} = this.state;
-    //       let urlBreed3 = "https://dog.ceo/api/breed/" + select + "/images/random/3";
-    //       axios
-    //       .get(urlBreed3)
-    //       .then(response => {
-    //           this.setState({
-    //             imgUrl3: response.data.message
-    //           });
-    //           console.log(response.data.message)
-    //       })
-    //       .catch(err => {
-    //         console.log("error fetching image");
-    //       });
-    //   }
+    getNumOfImg = () => {
+        const { selectVal } = this.state;
+        let url = ("https://dog.ceo/api/breeds/image/random/" + selectVal);
+        fetch(url)
+            .then((res) => res.json())
+            .then((data) => {
+                this.setState({ dogs: data.message })
+            })
+            .catch(err => {
+                console.log("error fetching image");
+            });
+    };
 
-
+    componentDidMount() {
+        this.getBreed();
+        // this.getSubBreed();
+        this.getNumOfImg();
+    }
 
     handleSelect = (e) => {
         this.setState({
@@ -97,29 +91,19 @@ class App extends React.Component {
         })
     }
 
-
-    // subBreed() {
-
-    // }
-
-
-    componentDidMount() {
-        this.getBreed();
-        let url = "https://dog.ceo/api/breeds/image/random/3";
-        fetch(url)
-            .then((res) => res.json())
-            .then((data) => {
-                this.setState({ dogs: data.message })
-            })
+    setSelectValue = (event) => {
+        this.setState({
+            selectVal: event.target.value
+        });
     }
 
     render() {
-        const { breed, imgURL, select, selectedOption } = this.state;
+        const { breed, imgURL, select, selectVal } = this.state;
 
         return (
             <div>
 
-                <h1 style={{textAlign: "center"}}>Welcome to eco surv exercise for dog.ceo</h1>
+                <h1 style={{ textAlign: "center" }}>Welcome to eco surv exercise for dog.ceo</h1>
 
                 <div class="con">
                     <p>Breed:</p>
@@ -128,25 +112,36 @@ class App extends React.Component {
                             <option value={e}> {e} </option>
                         )}
                     </select>
-                    
-                    {/*Where sub-breed comes in*/}
+{/* 
                     <p>Sub-Breed:</p>
-                    <Select value={selectedOption}
-                    onChange={this.handleChange}
-                    options={options2   }/>
-                    {/*End */}
+                    <select value={selectSub} onChange={this.handleSubSelect}>
+                        {Sub.map(x =>
+                            <option value={x}> {x} </option>
+                        )}
+                    </select> */}
+
 
                     <p>Number of Images:</p>
-                    <Select
-                        value={selectedOption}
-                        onChange={this.handleChange}
-                        options={options}
-                    />
+                    <select value={selectVal} onChange={this.setSelectValue}>
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5">5</option>
+                        <option value="6">6</option>
+                        <option value="7">7</option>
+                        <option value="8">8</option>
+                        <option value="9">9</option>
+                        <option value="10">10</option>
+                    </select>
+                    <p>{this.state.selectVal}</p>
+
                     <button id="submit" disabled={!select} onClick={this.getDogImage}>View Images</button>
+                    <button id="submit" disabled={!selectVal} onClick={this.getNumOfImg}>View Images</button>
                     <p></p>
 
                 </div>
-                
+
 
                 {/* Shows which type of breed */}
                 {/* <p>Breed: {select}</p> */}
